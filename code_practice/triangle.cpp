cf70af4f4d600033ad790c5074a91640fa994227
crazenator <crazenator@gmail.com>
2017-10-20 16:57:50 +0500

    Completed Triangle Solution

--- code_practice/triangle.cpp
+++ code_practice/triangle.cpp
@@ -13,32 +13,13 @@ int Triangle::Test(vector<int> & A)
     unsigned int i;
     unsigned int p, q, r;
 
-    vector<int> sA = A;
-    stable_sort(sA.begin(), sA.end());
+    stable_sort(A.begin(), A.end());
 
-    for (i = 0; i < (sA.size() - 1); i++)
+    for (i = 0; i < (A.size() - 2); i++)
     {
-        if (sA.at(i) + sA.at(i+1) > sA.at(i+3))
+        if (A.at(i) + A.at(i+1) > A.at(i+2))
         {
-            p = find(A.begin(), A.end(), A.at(i)) - A.begin();
-
-            if (sA.at(i) == sA.at(i+1))
-            {
-                q = find(A.begin()+p, A.end(), A.at(i+1)) - A.begin();
-            }
-            else
-            {
-                q = find(A.begin(), A.end(), A.at(i+1)) - A.begin();
-            }
-
-            if (sA.at(i+1) == sA.at(i+2))
-            {
-                r = find(A.begin()+q, A.end(), A.at(i+2)) - A.begin();
-            }
-            else
-            {
-                r = find(A.begin(), A.end(), A.at(i+2)) - A.begin();
-            }
+            return 1;
         }
     }
 
