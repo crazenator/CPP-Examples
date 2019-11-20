57b9caad4da8b5f43e96d3954ece791b38e9957a
Kamran Rauf <crazenator@gmail.com>
2019-11-20 14:52:44 +0100

    Added generic solution for PermCheck

--- code_practice/permcheck.cpp
+++ code_practice/permcheck.cpp
@@ -1,4 +1,4 @@
-#include <map>
+#include <vector>
 
 #include "permcheck.h"
 
@@ -7,8 +7,9 @@ using namespace std;
 
 PermCheck::PermCheck() {}
 
-int PermCheck::Test(vector<int> & A)
+int PermCheck::TestSolution1(vector<int> & A)
 {
+    // Assuming the permutation will start from 1
     int missingNum = 1;
     map<int, bool> dataMap;
 
@@ -26,4 +27,20 @@ int PermCheck::Test(vector<int> & A)
     }
 
     return (missingNum <= (int)A.size()) ? 0 : 1;
+}
+
+int PermCheck::TestSolution2(vector<int> & A)
+{
+    // Generic solution; no assumption on the start of A
+    int i = 0;
+    sort(A.begin(), A.end());
+    for (auto element : A)
+    {
+        if (element != (i + A.at(0)))
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
 }
